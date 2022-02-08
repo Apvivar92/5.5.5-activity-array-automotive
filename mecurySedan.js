10620739707e3b9bd387da99e847496d70debe6f
Paul  Vivar <Apvivar92@gmail.com>
2022-02-07 23:38:27 -0800

    created Car class that extends Vehicle

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,3 +1,46 @@
+import Vehicle from "vehicleBaseClass.js"
+
+class Car extends Vehicle{
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maximumPassengers = 6;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 200;
+        this.fuel = 100; 
+        this.scheduleService = false;
+    }
+    
+    start(){
+        if(this.fuel > 0){
+            console.log("Engine Started")
+        }
+        else {
+            return this.started = false;
+            console.log("Out of Gas");
+        }
+    }
+
+    loadPassenger(num){
+        if (this.passenger < this.maximumPassengers){
+            if((num + this.passenger) < this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;
+            }
+            else {
+                console.log("Not even space for Passengers")
+            }
+        }
+    }
+    
+    scheduleService(mileage){
+        if(this.mileage > 30000){
+            this.scheduleService == true
+            return this.scheduleService;
+        }
+    }
+}
+
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicle")
 
